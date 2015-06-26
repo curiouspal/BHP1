3c9c4dca4f7b6b7d1a1ad5e2877b65b232816e61
curiouspal <curiouspal@users.noreply.github.com>
2015-06-25 23:45:50 -0700

    More additions to data file locations.

--- ImportData.R
+++ ImportData.R
@@ -7,8 +7,8 @@ ph <- read.spss('/Data/PH.sav', to.data.frame=TRUE) ## Import PH data from SPSS 
 s8 <- read.spss('/Data/S8_NA_999.sav', to.data.frame=TRUE) ## Import S8 data from SPSS files.
 
 # Now load the additional data that BHP provided from their administrative database.
-phadditional <- read.csv("ph.csv")
-s8additional <- read.csv("s8.csv")  
+phadditional <- read.csv("/Data/BHP-Admin_records/ph.csv")
+s8additional <- read.csv("/Data/BHP-Admin_records/s8.csv")  
 
 #### CLEANING THE DATA FILES ####
 names(phadditional)[1] <- "location"
